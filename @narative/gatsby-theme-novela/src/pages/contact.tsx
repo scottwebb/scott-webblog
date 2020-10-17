91f893de347d3202b7efbc7a78f095162ef92183
Scott Webb <hello@scottwebb.me>
2020-10-17 00:19:06 -0400

    updating to test form

--- @narative/gatsby-theme-novela/src/pages/contact.tsx
+++ @narative/gatsby-theme-novela/src/pages/contact.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
 import mediaqueries from "@styles/media";
-
 import Section from "@components/Section";
 import SEO from "@components/SEO";
 import Image from '@components/Image';
@@ -26,20 +25,46 @@ const AboutPage: Template = ({ location, pageContext }) => {
         <ContentContainer>
           <MyImage><Image src={image} alt="Scott Webb, Photographer"/></MyImage>
           <MyText>
-          <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" placeholder="Your Email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+          <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="contact" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field"/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:<br />
+              <input type="text" name="name"/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email"/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message"/>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
           </MyText>
         </ContentContainer>
       </Section>
